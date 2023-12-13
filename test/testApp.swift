0adf833ffce006d2b446f1695809fe7364ce7d42
johngjackson0 <johngjackson0@gmail.com>
2023-12-13 14:14:37 -0500

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by John Jackson on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
